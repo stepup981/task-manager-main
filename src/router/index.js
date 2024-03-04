2fdbfe66aaa71da78a6b6386394b22d8cd4d1d7b
Roman Berdnikov <berdnikov.roma@gmail.com>
2024-03-04 23:02:10 +0300

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import MainPage from '../views/MainPage.vue'
-import TaskList from '../views/TaskList.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import MainPage from "@/views/MainPage.vue";
+import TaskList from "@/views/TaskList.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: MainPage
+    path: "/",
+    name: "home",
+    component: MainPage,
   },
   {
-    path: '/task',
-    name: 'task',
-    component: TaskList
-  }
-]
+    path: "/task",
+    name: "task",
+    component: TaskList,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
